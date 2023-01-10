e746e8779b5360540b0cb592b60fcf92af8227fb
Pascal Hurni <phi@pragmaticpurist.org>
2023-01-10 16:40:20 +0100

    Fix indentation

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,13 +7,13 @@ use Illuminate\Support\Carbon;
 
 class ServiceProvider extends BaseServiceProvider
 {
-	/**
-	 * Bootstrap the application services.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
         Carbon::macro('l', static function ($formatName = "datetime_default") {
             return self::this()->isoFormat(trans("datetime_formats.$formatName"));
         });
@@ -21,14 +21,14 @@ class ServiceProvider extends BaseServiceProvider
         $this->publishes([
             __DIR__.'../lang/en' => $this->app->langPath('en'),
         ]);
-	}
+    }
 
-	/**
-	 * Register the application services.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-	}
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
 }
